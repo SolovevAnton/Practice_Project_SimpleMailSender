3726aa4ef30ae38e09b18ad8579cff7837be9ef6
Solovev Anton <solovev.anton93@gmail.com>
2023-05-31 16:17:12 +0300

    added UI

--- src/main/java/com/solovev/simplecrmfx/controllers/MainController.java
+++ src/main/java/com/solovev/simplecrmfx/controllers/MainController.java
@@ -1,13 +1,17 @@
 package com.solovev.simplecrmfx.controllers;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController extends Application {
+    public TableView tableView;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("main.fxml"));
@@ -19,5 +23,8 @@ public class MainController extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void menuItemOpen(ActionEvent actionEvent) {
     }
 }
