5bb0ac0280f21ff9a0843488aed60d3ea92e4107
Solovev Anton <solovev.anton93@gmail.com>
2023-06-06 10:27:48 +0300

    added action buttons

--- src/main/java/com/solovev/simplecrmfx/App.java
+++ src/main/java/com/solovev/simplecrmfx/App.java
@@ -1,9 +1,14 @@
 package com.solovev.simplecrmfx;
 
+import com.solovev.simplecrmfx.controllers.ControllerData;
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -19,5 +24,69 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    /**
+     * Method to set up additional stage
+     *
+     * @param name  of the stage .fxml file
+     * @param title of the stage
+     * @param data  to pass to the stage, or null if nothing
+     * @param <T>
+     * @return created stage
+     * @throws IOException
+     */
+    public static <T> Stage getStage(String name, String title, T data) throws IOException {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(name));
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle(title);
+        if (data != null) {
+            ControllerData<T> controller = loader.getController();
+            controller.initData(data);
+        }
+        return stage;
+    }
+
+    /**
+     * Method to open stage without freezing the main one
+     *
+     * @param name  of the stage .fxml file
+     * @param title of the stage
+     * @param data  to pass to the stage, or null if nothing
+     * @return created stage
+     * @throws IOException
+     */
+    public static <T> Stage openWindow(String name, String title, T data) throws IOException {
+        Stage stage = getStage(name, title, data);
+        stage.show();
+        return stage;
+    }
+
+    /**
+     * Method to open stage freezing the main one
+     *
+     * @param name  of the stage .fxml file
+     * @param title of the stage
+     * @param data  to pass to the stage, or null if nothing
+     * @return created stage
+     * @throws IOException
+     */
+    public static <T> Stage openWindowAndWait(String name, String title, T data) throws IOException {
+        Stage stage = getStage(name, title, data);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+        return stage;
+    }
+
+    /**
+     * Method to close window
+     *
+     * @param event what triggers window closing
+     */
+    public static void closeWindow(Event event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
 }
